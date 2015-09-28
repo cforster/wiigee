5f2fc68212457923c05a98c7bac679aafda5a69e
biohazardpb4 <prombus@gmail.com>
2015-09-28 07:59:04 -0400

    partial: adding model saving and loading facilities to Android GUI

--- wiigee-gui-android/app/src/main/java/org/wiigee/android/MainActivity.java
+++ wiigee-gui-android/app/src/main/java/org/wiigee/android/MainActivity.java
@@ -1,10 +1,15 @@
 package org.wiigee.android;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.wiigee.control.AndroidWiigee;
@@ -13,12 +18,15 @@ import org.wiigee.event.GestureListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int GET_MODEL_INTENT_ID = 1;
+    private AndroidWiigee androidWiigee;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AndroidWiigee androidWiigee = new AndroidWiigee(this);
+        androidWiigee = new AndroidWiigee(this);
         androidWiigee.setTrainButton((Button) findViewById(R.id.buttonTrain));
         androidWiigee.setRecognitionButton((Button) findViewById(R.id.buttonRecognize));
         androidWiigee.setCloseGestureButton((Button) findViewById(R.id.buttonClose));
@@ -30,27 +38,43 @@ public class MainActivity extends AppCompatActivity {
                         + ", prob: " + gestureEvent.getProbability(), Toast.LENGTH_SHORT).show();
             }
         });
+
+        findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int modelNumber = Integer.parseInt((
+                        (EditText) findViewById(R.id.editTextModelNumber)).getText().toString());
+                String modelName = ((EditText) findViewById(R.id.editTextFileName))
+                        .getText().toString();
+                String modelFilePath = getFilesDir().getAbsolutePath() + "/" + modelName;
+                androidWiigee.getDevice().saveGesture(modelNumber, modelFilePath);
+                Toast.makeText(MainActivity.this, "Gesture saved to " + modelName,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.buttonLoad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, GET_MODEL_INTENT_ID);
+                }
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GET_MODEL_INTENT_ID && resultCode == RESULT_OK) {
+            Uri modelFileUri = data.getData();
+            String modelFilePath = modelFileUri.getPath();
+            androidWiigee.getDevice().loadGesture(modelFilePath);
+            Toast.makeText(MainActivity.this,
+                    "Gesture loaded: " + modelFileUri.getLastPathSegment(),
+                    Toast.LENGTH_SHORT).show();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
