4c90056dfee4eca14fa3737939ea3be75e380e44
biohazardpb4 <jay@jprodevelopment.com>
2015-09-28 23:04:17 -0400

    adding file saving and loading capability to Android GUI

--- wiigee-gui-android/app/src/main/java/org/wiigee/android/MainActivity.java
+++ wiigee-gui-android/app/src/main/java/org/wiigee/android/MainActivity.java
@@ -1,24 +1,23 @@
 package org.wiigee.android;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.wiigee.control.AndroidWiigee;
 import org.wiigee.event.GestureEvent;
 import org.wiigee.event.GestureListener;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 public class MainActivity extends AppCompatActivity {
 
-    private final int GET_MODEL_INTENT_ID = 1;
     private AndroidWiigee androidWiigee;
 
     @Override
@@ -56,25 +55,29 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.buttonLoad).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(intent, GET_MODEL_INTENT_ID);
-                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                final String[] files = getFilesDir().list(new FilenameFilter() {
+                    @Override
+                    public boolean accept(File dir, String filename) {
+                        return filename.endsWith(".txt");
+                    }
+                });
+                builder.setTitle("Pick a Model").setItems(files,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                String modelFilePath = files[which];
+                                // Wiigee appends .txt to model file paths, so strip it out here.
+                                androidWiigee.getDevice().loadGesture(modelFilePath.substring(0, modelFilePath.length() - 4));
+                                Toast.makeText(MainActivity.this,
+                                        "Gesture loaded: " + modelFilePath,
+                                        Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            }
+                        });
+                builder.create().show();
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == GET_MODEL_INTENT_ID && resultCode == RESULT_OK) {
-            Uri modelFileUri = data.getData();
-            String modelFilePath = modelFileUri.getPath();
-            androidWiigee.getDevice().loadGesture(modelFilePath);
-            Toast.makeText(MainActivity.this,
-                    "Gesture loaded: " + modelFileUri.getLastPathSegment(),
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
+    ;
 }
